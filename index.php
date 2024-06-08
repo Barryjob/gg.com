ece063ef3c0c7092b318c9a0823e1e3a7f6e0baf
Barry Job M <barrygodlikej@gmail.com>
2024-06-08 21:18:10 +0100

    08-06-24

---
+++ index.php
@@ -0,0 +1,158 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http: //www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php include "meta.php"?> 
+<title>zoze-demo.com </title>
+
+</head>
+<body>
+<section class="dasboard">
+    <div class="body-div">
+        <div class="body-in">
+
+            <div class="first-section">
+                <div class="inner">
+                    <h3>USER LINKS</h3>
+
+                    <div class="icon-txt">
+                        
+                        <ul>
+                            <li class="top">
+                                <a href="#" title="dashboard">
+                                <i class="bi bi-speedometer2"></i></a>
+                                <p>Dashboard</p>
+                            </li>
+
+                            <li>
+                                <a href="#" title="dashboard">
+                                <i class="bi bi-speedometer2"></i></a>
+                                <p>Dashboard</p>
+                            </li>
+
+                            <li>
+                                <a href="#" title="dashboard">
+                                <i class="bi bi-speedometer2"></i></a>
+                                <p>Dashboard</p>
+                            </li>
+
+                        </ul>
+                      
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="second-section">
+                <div class="profile">
+
+                   <div class="con-in">
+                        <div class="log-out">
+                            <p>USER PROFILE</p>
+                            <button class="btn">
+                            <a href="#" title="exit">
+                            <i class="bi bi-box-arrow-right"></i></a>
+                            <p>Log Out</p>
+                            </button>
+                        </div>
+                  
+
+                        <div class="pic-txt">
+                            <div class="pic">
+                                <img src="all-images/images/me.jpg" alt="profile picture">
+                            </div>
+                            <div class="txt">
+                                <p>Welcome Back!</p>
+                                <h2>Barry Job</h2>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="wallet-breakdown">
+                    <div class="inner-div">
+                        <div class="wallet-btn">
+                            <p>WALLET BALANCE</p>
+                            <button>
+                            <a href="#" title="load wallet">
+                            <i class="bi bi-wallet-fill"></i></a>
+                            <p>Load Wallet</p>
+                            </button>
+                        </div>
+
+                        <div class="amount">
+                            <div class="inner">
+                                <h3>USD 0.00</h3>
+                                <p>TOTAL AMOUT RECEIVED</p>
+                            </div>
+
+                            <div class="inner">
+                                <h3>USD 0.00</h3>
+                                <p>TOTAL AMOUT RECEIVED</p>
+                            </div>
+
+                            <div class="inner no-border">
+                                <h3>USD 0.00</h3>
+                                <p>TOTAL AMOUT RECEIVED</p>
+                            </div>
+
+                        </div>
+                    
+                        <div class="wallet-btn all-transaction">
+                            <p>WALLET TRANSACTION</p>
+                            <button class="view">
+                            <a href="#" title="view all">
+                            <i class="bi bi-eye"></i></a>
+                            <p>View All</p>
+                            </button>
+                        </div>
+
+                        <div class="table-div">
+
+                            <table>
+                                <tr>
+                                    <th>DATE</th>
+                                    <th>TRANS ID</th>
+                                    <th>BALANCE BEFORE</th>
+                                    <th>AMOUT LOADED</th>
+                                    <th>BALANCE AFTER</th>
+                                    <th>TRANS TYPE</th>
+                                    <th>STATUS</th>
+                                </tr>
+
+                                <tr>
+                                    <td>2024-12-23</td>
+                                    <td>TRC09085048474</td>
+                                    <td>$50,000</td>
+                                    <td>$1,400,000.00</td>
+                                    <td>$40,000</td>
+                                    <td>CREDI</td>
+                                    <td>SUCCESSFUL</td>
+                                </tr>
+
+                                <tr>
+                                    <td>2024-12-23</td>
+                                    <td>TRC09085048474</td>
+                                    <td>$50,000</td>
+                                    <td>$1,400,000.00</td>
+                                    <td>$40,000</td>
+                                    <td>CREDI</td>
+                                    <td>SUCCESSFUL</td>
+                                </tr>
+
+                            </table>
+
+                        </div>
+                        
+                    </div>
+
+                   
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+  
+</body>
+</html>
